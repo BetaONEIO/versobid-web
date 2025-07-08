877d3bdd3e4757d36fb8fa5ad6333af7bad3fd5f
a-mancodesss <079bct011.aman@pcampus.edu.np>
2025-07-08 21:50:43 +0545

    fix: dollar to GBP

--- src/pages/Listing/components/BidForm/BidForm.tsx
+++ src/pages/Listing/components/BidForm/BidForm.tsx
@@ -77,7 +77,7 @@ const BidForm: React.FC<BidFormProps> = ({ item }) => {
       <div>
         <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">Your Offer</label>
         <div className="relative mt-1">
-          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400">$</span>
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400">£</span>
           <input
             type="number"
             required
